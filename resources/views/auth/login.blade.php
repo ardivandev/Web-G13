e359f1559fde55137af17f317e065b15192f953d
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-09-15 13:50:35 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
-    <title>G13</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <title>G13</title>
 
     <style>
         body {
