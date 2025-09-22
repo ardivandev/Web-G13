13f4e962d0200dff5b37d864a3d49cb69bcd300f
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-09-22 09:01:15 +0700

    update Guru Import

--- app/Imports/GuruImport.php
+++ app/Imports/GuruImport.php
@@ -12,8 +12,8 @@ class GuruImport implements ToModel, WithHeadingRow
      * Mapping kemungkinan header Excel
      */
     protected $aliases = [
-        'nama_guru' => ['nama guru', 'nama', 'guru', 'nama lengkap', 'full name'],
-        'nip'       => ['nip', 'no induk pegawai', 'nomor induk', 'id pegawai', 'nomor nip'],
+        'nama_guru' => ['nama guru', 'nama', 'guru', 'nama lengkap', 'full name', 'Nama Guru', 'nama_guru'],
+        'nip'       => ['nip', 'no induk pegawai', 'nomor induk', 'id pegawai', 'nomor nip', 'NIP'],
     ];
 
     /**
@@ -22,19 +22,47 @@ class GuruImport implements ToModel, WithHeadingRow
     protected function getValue(array $row, string $field)
     {
         foreach ($this->aliases[$field] as $alias) {
-            $key = strtolower(str_replace(' ', '_', $alias)); // normalisasi key
-            if (isset($row[$key])) {
-                return $row[$key];
+            // Normalisasi: lowercase + hapus spasi
+            $key = strtolower(str_replace(' ', '', $alias));
+
+            foreach ($row as $rowKey => $value) {
+                $normalizedKey = strtolower(str_replace(' ', '', $rowKey));
+                if ($normalizedKey === $key) {
+                    return $value;
+                }
             }
         }
         return null;
     }
 
     public function model(array $row)
-    {
-        return new Guru([
-            'nama_guru' => $this->getValue($row, 'nama_guru'),
-            'nip'       => $this->getValue($row, 'nip'),
-        ]);
+{
+    $nama = $this->getValue($row, 'nama_guru');
+    $nip  = $this->getValue($row, 'nip');
+
+    // Debug sementara (tulis ke laravel.log)
+    \Log::info('Row Guru Import', [
+        'row'  => $row,
+        'nama' => $nama,
+        'nip'  => $nip,
+    ]);
+
+    if (empty($nama)) {
+        \Log::warning("Skip row karena nama kosong", $row);
+        return null;
     }
+
+    $nip = !empty($nip) ? $nip : null;
+
+    if ($nip && Guru::where('nip', $nip)->exists()) {
+        \Log::warning("Skip row karena NIP duplikat: " . $nip);
+        return null;
+    }
+
+    return new Guru([
+        'nama_guru' => $nama,
+        'nip'       => $nip,
+    ]);
+}
+
 }
