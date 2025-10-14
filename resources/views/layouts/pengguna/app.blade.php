9add41ee2f275e27740b2a42e4c213c4d3560e38
Ardivan Nur Raihan Rahman <ardivannurraihanr@gmail.com>
2025-10-14 13:38:45 +0700

    update re[p

--- resources/views/layouts/pengguna/app.blade.php
+++ resources/views/layouts/pengguna/app.blade.php
@@ -8,7 +8,8 @@
     <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
 
     {{-- Bootstrap CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     {{-- Google Fonts --}}
